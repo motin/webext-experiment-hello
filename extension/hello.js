e4df445779307b9a200baeb200a6f4849b2c1b4d
Fredrik Wollsén <fredrik@neam.se>
2018-03-03 19:01:17 +0200

    Copy Services.jsm from tree to the extension directory and add a Cu.import to hello.js

--- extension/hello.js
+++ extension/hello.js
@@ -4,6 +4,12 @@ this.hello = class extends ExtensionAPI {
     return {
       hello: {
         async hello() {
+
+          const resourceUrl = context.extension.getURL("Services.jsm");
+          console.log("resourceUrl to embedded Services.jsm: ", resourceUrl);
+          Cu.import(resourceUrl);
+          console.log("./Services.jsm loaded: ", Services);
+
           return "Hello, world!";
         }
       }

ac0f4f9d9aae23a706812d861cf5568a3c130a8b
Fredrik Wollsén <fredrik@neam.se>
2018-04-05 13:56:14 +0300

    Implemented an event

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,6 @@
+browser.hello.onSomething.addListener(param1 => {
+  console.log(`Something happened: ${param1}`);
+});
 
 browser.hello.hello().then(
   message => console.log(`hello sez: "${message}"`)
--- extension/hello.js
+++ extension/hello.js
@@ -1,12 +1,36 @@
+ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm");
+
+const { EventManager } = ExtensionCommon;
+const { EventEmitter } = ExtensionUtils;
+
+class HelloEventEmitter extends EventEmitter {
+  emitHelloEvent() {
+    console.log("Emitting hello-event from HelloEventEmitter");
+    this.emit("hello-event");
+  }
+}
 
 this.hello = class extends ExtensionAPI {
   getAPI(context) {
+    const helloEventEmitter = new HelloEventEmitter();
     return {
       hello: {
         async hello() {
+          helloEventEmitter.emitHelloEvent();
           return "Hello, world!";
         }
-      }
+      },
+      onSomething: new EventManager(context, "hello.onSomething", fire => {
+        const callback = value => {
+          console.log("Firing hello-event from hello experiments API");
+          fire.async(value);
+        };
+        helloEventEmitter.on("hello-event", callback);
+        return () => {
+          helloEventEmitter.off("hello-event", callback);
+        };
+      }).api()
     };
   }
 }
